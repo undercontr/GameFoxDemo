749e4ecff5bf20ab512ca126b19f51339fb600b8
undercontr <onder.alkan15@gmail.com>
2021-02-25 21:54:27 +0300

    initial commit

--- GameFoxDemo/IGamerManager.cs
+++ GameFoxDemo/IGamerManager.cs
@@ -6,5 +6,10 @@ namespace GameFoxDemo
 {
     interface IGamerManager
     {
+        public void RegisterAccount(IGamer gamer);
+        public void UpdateGamerInfo(IGamer gamer);
+        public void RemoveAccount(IGamer gamer);
+        public void AccountInfo(IGamer gamer);
+        public void TCKimlikValidation(IGamer gamer);
     }
 }
